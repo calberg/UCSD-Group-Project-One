123741d16f788b5b9ab1dd85a056a8c83252e195
xlmnope <lmgiroud@gmail.com>
2019-01-26 13:34:20 -0800

    updated UI of videos

--- assets/javascript.js
+++ assets/javascript.js
@@ -18,9 +18,6 @@ function handleResponse(response){
   $("#searchResults").html("");
   $(".results").html("");
   $(".results").append("<div class='row'><h6>Results</h6></div>");
-    
-  
-  searchresults = true
   for (var i = 0; i < 3; i++) {
     var item = response.items[i];
     // show title, author, and image in UI 
@@ -66,12 +63,13 @@ function generateYoutubeURL(){
       console.log(response);
       console.log(response.items[0].id.videoId);
       youtubeVideoID = response.items[0].id.videoId;
+      console.log(youtubeVideoID);
   })
   
 };
 
 //click handler to load youtube videos 
 $( "#searchResults" ).click(function() {
-  var player = $('<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/' + youtubeVideoID + '?autoplay=1&origin=http://example.com" frameborder="0"></iframe>');
+  var player = $('<div class="col s4"><div class="card"><div class="card-image video"><iframe id="ytplayer" type="text/html" width="200" height="" src="https://www.youtube.com/embed/' + youtubeVideoID +'?autoplay=1&origin=http://example.com"frameborder="0"></iframe></div></div></div>');
   $('#player').append(player);
 });
